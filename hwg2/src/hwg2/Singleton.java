2ee399ecb5bfad03a0516068312e4e9ace2ebd2c
HWG2001 <ghadah841@gmail.com>
2022-11-08 19:59:44 +0300

    f3

--- hwg2/src/hwg2/Singleton.java
+++ hwg2/src/hwg2/Singleton.java
@@ -17,6 +17,21 @@ public class Singleton {
 	return uniqueInstance;
 	}
 	// other useful methods here
+private int Num;
+	
+	public int getNum() {
+		return this.Num ; 
+	}
+	
+	public void setNum(int Number) {
+		if (Number>0) {
+			this.Num = Number ; 
+			System.out.println("this number is positive" +  Number);
+		}
+		else {
+			System.out.println("it is negtive");
+		}
+	}
 	}
 
 
